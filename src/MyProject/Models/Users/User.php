bbe73e23908d2bfb3dda2b27e65e3feffa518d56
ArturM2005 <arturmuhamediarov@gmail.com>
2025-01-08 04:28:03 +0400

    second commit

--- src/MyProject/Models/Users/User.php
+++ src/MyProject/Models/Users/User.php
@@ -1,7 +1,35 @@
 <?php
 
+
+
 namespace MyProject\Models\Users;
+
+
+
 class User
+
 {
+
+    private $name;
+
+
+
+    public function __construct(string $name)
+
+    {
+
+        $this->name = $name;
+
+    }
+
+
+
+    public function getName(): string
+
+    {
+
+        return $this->name;
+
+    }
 
 }
